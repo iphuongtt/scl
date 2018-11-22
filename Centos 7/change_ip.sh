a41b3be92a9929df37b11d495657207d95a4e5ff
Trinh Thanh Phuong <iphuongtt@gmail.com>
2018-11-22 16:34:26 +0700

    test

--- Centos 7/change_ip.sh
+++ Centos 7/change_ip.sh
@@ -1,3 +1,11 @@
 #!bin/bash
 
-echo "Hello"
+if [ $(id -u) != "0" ]; then
+    printf "Ban can chay cau lenh nay bang tai khoan root"
+    exit
+fi
+printf "Nhap dia chi ip muon tao"
+
+read ipaddr
+
+sed -i ~/ifcfg-enp0s3.bak "s/IPADDR=.*/IPADDR=${ipaddr}/g" /etc/sysconfig/network-scripts/ifcfg-enp0s3
